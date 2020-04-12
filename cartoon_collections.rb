716dd80847c3e861ce9ca2b5bc4d82e8b0c99762
Nandita Venkat <nanditavenkat@gmail.com>
2020-04-12 23:37:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,4 +20,5 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find do |elem|
    puts valid_calls.include? elem
+ end
 end
